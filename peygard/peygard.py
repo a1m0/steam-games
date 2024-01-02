f59fe946e213decdcf80fc8fa5945c837cc46c62
almo99 <aliremo7899@gmail.com>
2024-01-02 17:55:34 +0330

    feat(peygard): add get_app_reviews
    Added get_app_reviews method for getting a games reviews given its app id.

--- peygard/peygard.py
+++ peygard/peygard.py
@@ -4,11 +4,14 @@ import os
 
 
 class Peygard:
-    def __init__(self, app_list_url, app_list_file_dir, app_details_url):
+    def __init__(
+        self, app_list_url, app_list_file_dir, app_details_url, app_reviews_url
+    ):
         self.app_list_url = app_list_url
         self.app_list_file_dir = app_list_file_dir
         self.app_list = None
         self.app_details_url = app_details_url
+        self.app_reviews_url = app_reviews_url
 
     def get_app_list(self):
         if self.app_list_file_dir is not None:
@@ -31,6 +34,38 @@ class Peygard:
         )
 
         with open(os.path.join("data", f"{app_id}.json"), "w") as app:
+            data = response.json()
+            json.dump(data, app)
+
+        return response
+
+    def get_app_reviews(
+        self,
+        app_id,
+        filter="recent",
+        language="all",
+        cursor="*",
+        review_type="all",
+        purchase_type="steam",
+        num_per_page=100,
+        filter_offtopic_activity=0,
+    ):
+        print("requestng", self.app_reviews_url, app_id)
+        response = requests.get(
+            f"{self.app_reviews_url}{app_id}",
+            params={
+                "json": 1,
+                "filter": filter,
+                "language": language,
+                "cursor": cursor,
+                "review_type": review_type,
+                "purchase_type": purchase_type,
+                "num_per_page": num_per_page,
+                "filter_offtopic_activity": filter_offtopic_activity,
+            },
+        )
+
+        with open(os.path.join("data", f"{app_id}-reviews.json"), "w") as app:
             data = response.json()
             json.dump(data, app)
 
