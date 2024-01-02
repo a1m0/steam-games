8c05a997fe4d016da5babc2a92243d62e57150e4
almo99 <aliremo7899@gmail.com>
2024-01-02 16:45:29 +0330

    feat(peygard): add get_app_details
    Added get_app_details method to Peygard class for getting details of a game
    given their app_id

--- peygard/peygard.py
+++ peygard/peygard.py
@@ -1,12 +1,14 @@
 import requests
 import json
+import os
 
 
 class Peygard:
-    def __init__(self, app_list_url, app_list_file_dir):
+    def __init__(self, app_list_url, app_list_file_dir, app_details_url):
         self.app_list_url = app_list_url
         self.app_list_file_dir = app_list_file_dir
         self.app_list = None
+        self.app_details_url = app_details_url
 
     def get_app_list(self):
         if self.app_list_file_dir is not None:
@@ -21,3 +23,15 @@ class Peygard:
                 json.dump(data, app_list_file)
 
             return response
+
+    def get_app_details(self, app_id, currency="us", language="en"):
+        response = requests.get(
+            self.app_details_url,
+            params={"appids": app_id, "cc": currency, "l": language},
+        )
+
+        with open(os.path.join("data", f"{app_id}.json"), "w") as app:
+            data = response.json()
+            json.dump(data, app)
+
+        return response
