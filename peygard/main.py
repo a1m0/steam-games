27f16b265786419fde37073300b9412613caee2f
almo99 <aliremo7899@gmail.com>
2024-01-04 14:13:06 +0330

    feat(peygard): Update main for get_app_players_history

--- peygard/main.py
+++ peygard/main.py
@@ -1,6 +1,5 @@
 from peygard import Peygard
 
-print("who")
 p = Peygard(
     app_list_url="https://api.steampowered.com/ISteamApps/GetAppList/v0002/",
     app_list_file_dir="data/app_list.json",
@@ -8,8 +7,11 @@ p = Peygard(
     app_reviews_url="https://store.steampowered.com/appreviews/",
     app_achievements_url="https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v0002/",
     app_achievements_gp_url="https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/",
+    app_players_history_url="https://steamcharts.com/app/",
 )
+
 # p.get_app_list()
 # print(p.app_list)
 # print(p.get_app_reviews(app_id=1086940, language="english").json())
-p.get_app_achievements_info(app_id=1086940)
+# p.get_app_achievements_info(app_id=1086940)
+p.get_app_players_history(app_id=1086940)
